69e7da0022cb9fdfa7e8c2d5bba2aaa6fb932ccc
ninions15 <ninopaul.cervantes@ustp.edu.ph>
2022-10-10 17:50:38 +0800

    init

--- components/tickets.js
+++ components/tickets.js
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useState } from 'react'
+
+const TicketItem = ({ item }) => {
+  return (
+    <View style={{ padding: 15, borderRadius: 10, borderColor: 'black', borderWidth: 1, marginVertical: 5 }}>
+      <Text>{item.title}</Text>
+      <Text>{item.description}</Text>
+    </View>
+  )
+}
 
 const Tickets = () => {
+  const [tickets, setTickets] = useState([{
+    _id: '1',
+    title: 'Test 1',
+    description: 'Description 1',
+  }, {
+    _id: '2',
+    title: 'Test 2',
+    description: 'Description 2',
+  }, {
+    _id: '3',
+    title: 'Test 3',
+    description: 'Description 3',
+  }, {
+    _id: '4',
+    title: 'Test 4',
+    description: 'Description 4',
+  }, {
+    _id: '5',
+    title: 'Test 5',
+    description: 'Description 5',
+  }]);
+
   return (
     <View>
-      <Text>Tickets</Text>
+      <FlatList
+        data={tickets}
+        renderItem={TicketItem}
+      />
     </View>
   )
 }
